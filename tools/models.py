24a3ab766da2deb1ba43e4fb0ec54f92981f270c
Kaleb Davis <kalebdavisgithub@gmail.com>
2016-09-18 14:45:35 -0400

    Add to string method for the Tool model

--- tools/models.py
+++ tools/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 class Tool(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=255)
     isAvailable = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.name)
