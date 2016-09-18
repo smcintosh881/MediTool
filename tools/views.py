a6b4dfa12473470482a0123df23af1b55cdbca43
Kaleb Davis <kalebdavisgithub@gmail.com>
2016-09-18 15:25:17 -0400

    Add detail page for Tool viewing

--- tools/views.py
+++ tools/views.py
@@ -1,7 +1,11 @@
 from tools.models import Tool
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     tools = Tool.objects.all()
     return render(request, 'tools/index.html', {'tools': tools})
+
+def detail(request, tool_id):
+    tool = get_object_or_404(Tool, pk=tool_id)
+    return render(request, 'tools/detail.html', {'tool': tool})
