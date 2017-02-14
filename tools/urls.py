7ebac55deac06c1f0600859af9a151217a6a0481
Kaleb Davis <kalebdavisgithub@gmail.com>
2017-02-14 14:47:37 -0500

    Add fill in the blanks for urls

--- tools/urls.py
+++ tools/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<tool_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^create/$', views.CreateTool.as_view(success_url="/"), name='create'),
-    url(r'^edit/(?P<pk>[0-9]+)/$', views.EditTool.as_view(success_url="/"), name='edit')
+    url(r'^edit/__FILL_ME_IN__/$', views.__FILL_ME_IN__.as_view(success_url="__FILL_ME_IN__"), name='edit')
 ]
